64564125659eb7e72c1eb8f2cceca77d1b918a59
gopalkashyap19 <gopal267kashyap@gmail.com>
2024-11-28 20:59:28 +0530

    These are 10 questions of tuple.

---
+++ coding-assignment/tuple/tuple_q02.py
@@ -0,0 +1,4 @@
+#Q2 =  Concatenate a tuple with itself three times:
+
+my_tuple = ('a', 'b')
+concatenated_tuple = my_tuple * 3  # Output: ('a', 'b', 'a', 'b', 'a', 'b')
---
+++ coding-assignment/tuple/tuple_q08.py
@@ -0,0 +1,4 @@
+#Q8 =  Replace the second element of a tuple without modifying it directly:
+
+t = (5, 10, 15)
+new_tuple = t[:1] + (20,) + t[2:]  # Output: (5, 20, 15)
